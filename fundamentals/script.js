24e342178b83c1a821093ef269a5a791a2eefb2b
MarkKoz <KozlovMark@gmail.com>
2018-03-10 09:48:04 -0800

    Completed exercise 1 (helloprog). Changed name in fundamentals.

--- fundamentals/script.js
+++ fundamentals/script.js
@@ -3,7 +3,7 @@ var mySecondValue = 7 + 9;
 var myThirdValue = myValue + mySecondValue;
 alert(myThirdValue);
 
-var myName = "Jesse";
-var myLastName = "Liberty";
+var myName = "Mark";
+var myLastName = "Kozlov";
 var myFullName = myName + " " + myLastName;
 alert(myFullName);
---
+++ helloprog/script.js
@@ -0,0 +1,3 @@
+const forename = prompt("What is your forename?");
+const surname = prompt("What is your surname?");
+alert(`Hello ${forename} ${surname}, How is your day going?`);
