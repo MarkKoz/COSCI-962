d7b2d82d3d5858d094dd77d84edfc4477266b315
MarkKoz <KozlovMark@gmail.com>
2018-03-10 08:58:32 -0800

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# CO SCI 962 - Project 2
+**Course Title:** Web Programming Using JavaScript<br/>
+**Section:** 10036<br/>
+**Instructor:** Manish Patel<br/>
+**Semester:** Spring 2018 (2018-02-05 to 2018-06-04)<br/>
+
+### Tasks
+
---
+++ fundamentals/script.js
@@ -0,0 +1,9 @@
+var myValue = 5;
+var mySecondValue = 7 + 9;
+var myThirdValue = myValue + mySecondValue;
+alert(myThirdValue);
+
+var myName = "Jesse";
+var myLastName = "Liberty";
+var myFullName = myName + " " + myLastName;
+alert(myFullName);
