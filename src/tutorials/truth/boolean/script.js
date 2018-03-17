cd64808051be60830fb7f46b72f10cc6f8341f85
MarkKoz <KozlovMark@gmail.com>
2018-03-16 21:49:04 -0700

    Completed boolean tutorial.

---
+++ src/tutorials/truth/boolean/script.js
@@ -0,0 +1,11 @@
+/*var isMale = true;
+var isOld = true;
+var isOldMan isMale && isOld;
+alert(isOldMan);*/
+
+var isFemale = false;
+var isOld = true;
+// var isOldOrFemale = isOld || isFemale;
+// alert(isOldOrFemale);
+var isMale = !isFemale;
+alert(isMale);
