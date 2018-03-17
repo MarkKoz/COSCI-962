9113e69d61e08d7f6916b3b3e6e09f10e55f047b
MarkKoz <KozlovMark@gmail.com>
2018-03-16 21:57:49 -0700

    Completed truthyness tutorial.

---
+++ src/tutorials/truth/truthyness/script.js
@@ -0,0 +1,22 @@
+/*alert(0 == false);
+alert(0 === false);
+alert(1 == false);*/
+
+/*var x = 5;
+x++;
+alert(x);*/
+
+/*var x = 5;
+var y = 6;
+var z = x + y;
+alert(z);*/
+
+var x = 5;
+// var y = x < 10 ? x : 10;
+var y;
+
+if (x < 10) {
+    y = x;
+} else {
+    y = 10;
+}
