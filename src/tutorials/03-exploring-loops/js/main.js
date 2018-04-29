3264b37271c7b96d18a2380b5e77db6da800debb
MarkKoz <KozlovMark@gmail.com>
2018-04-29 16:59:37 -0700

    Completed module 3 exercise 4.

--- src/tutorials/03-exploring-loops/js/main.js
+++ src/tutorials/03-exploring-loops/js/main.js
@@ -1,7 +1,15 @@
-var elementID = document.getElementById("state");
-console.log(elementID);
+function showStuff(el) {
+    var elementID = document.getElementById(el);
+    console.log(elementID);
 
-for (i = 0; i < elementID.length; ++i) {
-    console.log(elementID[i].text);
-    console.log(elementID[i].value);
+    for (i = 0; i < elementID.length; ++i) {
+        console.log(elementID[i].text);
+        console.log(elementID[i].value);
+        console.log(elementID[i].selected);
+    }
+}
+
+function evaluatePage() {
+    showStuff("state");
+    showStuff("home");
 }
