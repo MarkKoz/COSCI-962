f5a6757565a1e464317942eff95df8bdcfd8bedc
MarkKoz <KozlovMark@gmail.com>
2018-04-29 17:15:26 -0700

    Completed module 5 exercise 3.

--- src/tutorials/05-interrogating-dom/js/main.js
+++ src/tutorials/05-interrogating-dom/js/main.js
@@ -7,3 +7,8 @@ foundYou = document.getElementsByTagName("p");
 console.log(foundYou);
 console.log(foundYou[1].innerHTML);
 foundYou[1].innerHTML = ">>I Changed You<<";
+
+foundYou = document.getElementsByName("description");
+console.log(foundYou);
+console.log(foundYou[0].innerHTML);
+foundYou[0].innerHTML = ">>I Changed You<<";
