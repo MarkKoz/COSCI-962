004500031e1222981a967bcf752273406eec764c
MarkKoz <KozlovMark@gmail.com>
2018-04-29 17:05:02 -0700

    Completed module 3 exercise 5.

--- src/tutorials/03-exploring-loops/js/main.js
+++ src/tutorials/03-exploring-loops/js/main.js
@@ -1,15 +1,22 @@
 function showStuff(el) {
     var elementID = document.getElementById(el);
-    console.log(elementID);
+    // console.log(elementID);
 
     for (i = 0; i < elementID.length; ++i) {
-        console.log(elementID[i].text);
-        console.log(elementID[i].value);
-        console.log(elementID[i].selected);
+        // console.log(elementID[i].text);
+        // console.log(elementID[i].value);
+        // console.log(elementID[i].selected);
+
+        if (elementID[i].selected === true)
+            var x = elementID[i].text;
     }
+
+    return x;
 }
 
 function evaluatePage() {
-    showStuff("state");
-    showStuff("home");
+    var feedbackState = showStuff("state");
+    var feedbackHome = showStuff("home");
+
+    document.getElementById("output").innerHTML = feedbackState + "<br>" + feedbackHome;
 }
