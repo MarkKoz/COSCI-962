3e6acddacb2fa83f99930fdae04b37898dfeb3dd
MarkKoz <KozlovMark@gmail.com>
2018-04-29 17:30:14 -0700

    Completed module 5 exercise 8.

--- src/tutorials/05-interrogating-dom/js/main.js
+++ src/tutorials/05-interrogating-dom/js/main.js
@@ -70,3 +70,12 @@ for (i = 0; i < selectArray.length; ++i)
 
 // 08 - Combination 3
 console.log("\n08 - Combination 3");
+
+foundYou = document.getElementById("myForm");
+console.log(foundYou);
+
+var selectArray = foundYou.getElementsByTagName("input");
+console.log(selectArray);
+
+for (i = 0; i < selectArray.length; ++i)
+    console.log(selectArray[i].value);
