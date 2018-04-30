2667e0ecf0cad97a9ebcf98c5291cf6bef9a6f98
MarkKoz <KozlovMark@gmail.com>
2018-04-29 18:42:54 -0700

    Completed payroll calculator.

--- src/payrollform/js/main.js
+++ src/payrollform/js/main.js
@@ -1,2 +1,57 @@
-/*jslint browser:true */
 "use strict";
+
+const form = document.forms.payrollForm;
+
+function getHours() {
+    // Converted to array so that reduce() can be used.
+    const inputs = Array.from(form.wts.getElementsByTagName("input"));
+
+    // Sums all the values of the inputs. Initial value of 0 so it starts at
+    // 0th index isntead of 1st. If the value can't be parsed (e.g. when input
+    // is empty), adds 0 instead, effectively ignoring it.
+    return inputs.reduce((acc, val) => acc + (parseInt(val.value) || 0), 0);
+}
+
+function getTaxRate() {
+    const select = form.panel;
+
+    return select.options[select.selectedIndex].value / 100;
+}
+
+function getGrossPay(hours, wage) {
+    var overtimePay = 0;
+
+    if (hours > 40) {
+        overtimePay = getOvertimePay(hours - 40, wage);
+        hours = 40;
+    }
+
+    return hours * wage + overtimePay;
+}
+
+function getOvertimePay(hours, wage) {
+    return hours * wage * 1.5;
+}
+
+function getTax(grossPay, taxRate) {
+    return grossPay * taxRate;
+}
+
+function getNetPay(grossPay, tax) {
+    return grossPay - tax;
+}
+
+function calculateSolar() {
+    const hours = getHours();
+    const gross = getGrossPay(hours, 12.50);
+    const tax = getTax(gross, getTaxRate());
+    const netPay = getNetPay(gross, tax);
+
+    document.getElementById("feedback").innerHTML =
+`<p>
+    Name: John Doe<br>
+    Total hours: ${hours}<br>
+    Tax: ${tax}<br>
+    Net pay: ${netPay}
+</p>`;
+}
