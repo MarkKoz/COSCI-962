bc326d9ba051dc3dacaccaa7a4b238c1e4795fdb
MarkKoz <KozlovMark@gmail.com>
2018-04-29 17:17:39 -0700

    Completed module 5 exercise 4.

--- src/tutorials/05-interrogating-dom/js/main.js
+++ src/tutorials/05-interrogating-dom/js/main.js
@@ -6,9 +6,13 @@ foundYou.innerHTML = ">>I Changed You<<";
 foundYou = document.getElementsByTagName("p");
 console.log(foundYou);
 console.log(foundYou[1].innerHTML);
-foundYou[1].innerHTML = ">>I Changed You<<";
+foundYou[1].innerHTML = "..I Changed You<<";
 
 foundYou = document.getElementsByName("description");
 console.log(foundYou);
 console.log(foundYou[0].innerHTML);
 foundYou[0].innerHTML = ">>I Changed You<<";
+
+foundYou = document.getElementsByClassName("smallCenter");
+console.log(foundYou[0].innerText);
+foundYou[0].innerText = "<<New Paragraph Info<<";
