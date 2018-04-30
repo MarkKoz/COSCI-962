7123189fa4ad20136840e6c130da5c58a0268429
MarkKoz <KozlovMark@gmail.com>
2018-04-29 17:38:05 -0700

    Completed module 6 exercise 1.

--- src/tutorials/06-solar-calculator/js/main.js
+++ src/tutorials/06-solar-calculator/js/main.js
@@ -1,2 +1,15 @@
-/*jslint browser:true */
 "use strict";
+
+function addMonths(elem) {
+    var annualUseKw = 0;
+    var months = document.getElementById(elem).getElementsByTagName("input");
+
+    for (let i = 0; i < months.length; ++i) {
+        annualUseKw += Number(months[i].value);
+    }
+
+    return annualUseKw / 365;
+}
+
+var dailyUseKw = addMonths("mpc");
+console.log(dailyUseKw);
