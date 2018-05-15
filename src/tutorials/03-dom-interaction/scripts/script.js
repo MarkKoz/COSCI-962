ea9073d4f023f7a985fca869412bcb29d77c3a33
MarkKoz <KozlovMark@gmail.com>
2018-05-15 16:15:15 -0700

    Completed exercise 03-05.

--- src/tutorials/03-dom-interaction/scripts/script.js
+++ src/tutorials/03-dom-interaction/scripts/script.js
@@ -1,10 +1,32 @@
 $(document).ready(() => {
+    const divs = $("div.BlueDiv, div.RedDiv");
     let html = "";
 
-    $("div.BlueDiv, div.RedDiv").each((i, e) => {
+    // Iterating nodes.
+    divs.each((i, e) => {
         html+= "<br/>" + i + $(e).text();
     });
 
-    const output = $("#OutputDiv");
-    output.html(html);
+    $("#OutputDiv").html(html);
+
+    // Modifying properties and attributes.
+    divs.each((i, e) => {
+        e.title = "Some title"; // Raw DOM object.
+        $(e).attr("title", "Some title 2");
+    });
+
+    // divs.css("font-size", "20pt");
+
+    /*divs.attr({
+        title: "Some title 3",
+        style: "font-size: 14pt; background-color: yellow; color: black;"
+    });*/
+
+    divs
+        .attr({
+            title: "Some title 3"
+        })
+        .css("background-color", "yellow")
+        .css("font-size", "14pt")
+        .css("color", "black");
 });
