690bf642e69bbd3583106a5af78e542546b171a2
MarkKoz <KozlovMark@gmail.com>
2018-05-15 14:38:14 -0700

    Completed exercise 02-03.

---
+++ src/tutorials/02-jquery-selectors/scripts/script.js
@@ -0,0 +1,16 @@
+$(document).ready(() => {
+    /*let imgs = $("img");
+    alert(imgs.length);*/
+
+    /*let divs = $("div");
+    alert(divs.length);*/
+
+    // $("div").css("background-color", "green");
+
+    /*$("div").each(function() {
+        alert($(this).html());
+    });*/
+
+    let coll = $("div, span");
+    alert(coll.length);
+});
