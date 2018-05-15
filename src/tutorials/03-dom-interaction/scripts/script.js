9eab2634919d812c3c0e88baee00af4c009c1e1d
MarkKoz <KozlovMark@gmail.com>
2018-05-15 16:07:21 -0700

    Completed exercise 03-03.

--- src/tutorials/03-dom-interaction/scripts/script.js
+++ src/tutorials/03-dom-interaction/scripts/script.js
@@ -1,3 +1,10 @@
 $(document).ready(() => {
+    let html = "";
 
+    $("div.BlueDiv, div.RedDiv").each((i, e) => {
+        html+= "<br/>" + i + $(e).text();
+    });
+
+    const output = $("#OutputDiv");
+    output.html(html);
 });
