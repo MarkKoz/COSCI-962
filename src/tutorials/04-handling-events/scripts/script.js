7a662b82ca43c2a4fb3a3a1d977383842f112bd7
MarkKoz <KozlovMark@gmail.com>
2018-05-15 17:31:42 -0700

    Completed exercise 04-05.

--- src/tutorials/04-handling-events/scripts/script.js
+++ src/tutorials/04-handling-events/scripts/script.js
@@ -1,3 +1,12 @@
 $(document).ready(() => {
-
+    WireEvents();
 });
+
+function WireEvents() {
+    $("#SubmitButton").click(() => {
+        const firstNameVal = $("#FirstNameTextBox").val();
+        const lastNameVal = $("#LastNameTextBox").val();
+
+        $("#DivOutput").text(firstNameVal + " " + lastNameVal);
+    });
+}
