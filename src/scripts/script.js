40b26dfbd451cb0dd05656ae12d8066e0aba67a1
MarkKoz <KozlovMark@gmail.com>
2018-06-01 13:22:05 -0700

    Implemented script.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,3 +1,43 @@
 $(document).ready(() => {
+    $("form").submit(false);
+    const balance = $(".balance");
 
+    // Hides feedback message when the input changes.
+    $("form input").on("input", function() {
+        $(this).closest("fieldset").find(".feedback").hide();
+    });
+
+    $("#deposit button").click(() => {
+        const target = $("#deposit");
+        const input = parseFloat(target.find("input").val());
+
+        balance.html((parseFloat(balance.html()) + input).toFixed(2));
+        setFeedback(target.find(".feedback"), `Successfully deposited $${input}.`);
+    });
+
+    $("#withdraw button").click(() => {
+        const target = $("#withdraw");
+        const balFloat = parseFloat(balance.html());
+        const input = parseFloat(target.find("input").val());
+
+        if (input > balFloat) {
+            setFeedback(target.find(".feedback"), `Withdrawal failed. A withdrawal of $${input} would result in the account being overdrawn.`, true);
+        } else {
+            balance.html((balFloat - input).toFixed(2));
+            setFeedback(target.find(".feedback"), `Successfully withdrew $${input}.`);
+        }
+    });
 });
+
+function setFeedback(e, msg, isError) {
+   if (isError) {
+       e.removeClass("success");
+       e.addClass("error");
+   } else {
+       e.addClass("success");
+       e.removeClass("error");
+   }
+
+    e.html(msg);
+    e.show();
+}
